51bb4e161169dbcb71d6e1fb3978ff9823a62769
robertzml <robertzml@126.com>
2019-11-09 16:07:13 +0800

    remove hermes-company module. remove hermes-gateway module. add hermes-account module.
    Signed-off-by: robertzml <robertzml@126.com>

---
+++ hermes-account/src/main/java/com/shengdangjia/hermesaccount/HermesAccountApplication.java
@@ -0,0 +1,13 @@
+package com.shengdangjia.hermesaccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HermesAccountApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HermesAccountApplication.class, args);
+    }
+
+}
