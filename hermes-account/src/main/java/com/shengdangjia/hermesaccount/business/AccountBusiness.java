9a10069f1d3a36aff672dd7e7a76e7f9e96399bc
robertzml <robertzml@126.com>
2019-11-19 20:06:04 +0800

    add redis support. test save to redis.
    Signed-off-by: robertzml <robertzml@126.com>

--- hermes-account/src/main/java/com/shengdangjia/hermesaccount/business/AccountBusiness.java
+++ hermes-account/src/main/java/com/shengdangjia/hermesaccount/business/AccountBusiness.java
@@ -4,13 +4,23 @@ import com.shengdangjia.hermesaccount.entity.Account;
 import com.shengdangjia.hermesaccount.repository.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 @Configuration
 public class AccountBusiness {
     @Autowired
     AccountRepository accountRepository;
 
+    @Autowired
+    StringRedisTemplate stringRedisTemplate;
+
     public Iterable<Account> findAll() {
         return accountRepository.findAll();
+    }
+
+    public boolean register(String token) {
+
+        stringRedisTemplate.opsForValue().set(token, "223344");
+        return true;
     }
 }
--- hermes-account/src/main/java/com/shengdangjia/hermesaccount/controller/AccountController.java
+++ hermes-account/src/main/java/com/shengdangjia/hermesaccount/controller/AccountController.java
@@ -4,6 +4,7 @@ import com.shengdangjia.hermesaccount.business.AccountBusiness;
 import com.shengdangjia.hermesaccount.entity.Account;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,5 +15,10 @@ public class AccountController {
     @RequestMapping("/account/list")
     Iterable<Account> getAll() {
         return accountBusiness.findAll();
+    }
+
+    @RequestMapping("/account/register")
+    boolean register(@RequestParam(value = "token")String token) {
+        return accountBusiness.register(token);
     }
 }
