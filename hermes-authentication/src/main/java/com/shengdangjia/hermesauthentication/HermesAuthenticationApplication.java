bd175942eeb04d7698b9f72c1b783b8fd110376f
robertzml <robertzml@126.com>
2019-12-07 15:22:25 +0800

    add hermes-authentication module. update sprint boot to 2.2.2.
    Signed-off-by: robertzml <robertzml@126.com>

---
+++ hermes-authentication/src/main/java/com/shengdangjia/hermesauthentication/HermesAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.shengdangjia.hermesauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HermesAuthenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HermesAuthenticationApplication.class, args);
+    }
+
+}
---
+++ hermes-authentication/src/test/java/com/shengdangjia/hermesauthentication/HermesAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shengdangjia.hermesauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HermesAuthenticationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
