c0a151a1b11c5631961ca82c4a11d70bd5e0658b
robertzml <robertzml@126.com>
2019-12-18 20:01:30 +0800

    add hermes-gateway module.
    Signed-off-by: robertzml <robertzml@126.com>

---
+++ hermes-gateway/src/test/java/com/shengdangjia/hermesgateway/HermesGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shengdangjia.hermesgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HermesGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
