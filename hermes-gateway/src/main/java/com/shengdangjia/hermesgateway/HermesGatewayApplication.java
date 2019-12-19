4185b3c8db92557d1c271c27ae1268496130a3b6
robertzml <robertzml@126.com>
2019-12-19 20:03:29 +0800

    edit gateway, can access service.
    Signed-off-by: robertzml <robertzml@126.com>

--- hermes-gateway/src/main/java/com/shengdangjia/hermesgateway/HermesGatewayApplication.java
+++ hermes-gateway/src/main/java/com/shengdangjia/hermesgateway/HermesGatewayApplication.java
@@ -3,6 +3,9 @@ package com.shengdangjia.hermesgateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -12,4 +15,21 @@ public class HermesGatewayApplication {
         SpringApplication.run(HermesGatewayApplication.class, args);
     }
 
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(p -> p
+                        .path("/account-service/**")
+                        .filters(
+                                f -> f.stripPrefix(1)
+                        )
+                        .uri("lb://hermes-account"))
+                .route(p -> p
+                        .path("/authentication-service/**")
+                        .filters(
+                                f -> f.stripPrefix(1)
+                        )
+                        .uri("lb://hermes-authentication"))
+                .build();
+    }
 }
