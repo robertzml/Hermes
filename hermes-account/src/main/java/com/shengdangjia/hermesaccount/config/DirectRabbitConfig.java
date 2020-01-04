d7a1c2cb18afbf7562d76000e725d4c2a158bc13
robertzml <robertzml@126.com>
2020-01-04 15:14:02 +0800

    import rabbitmq. test send package to rabbitmq.
    Signed-off-by: robertzml <robertzml@126.com>

---
+++ hermes-account/src/main/java/com/shengdangjia/hermesaccount/config/DirectRabbitConfig.java
@@ -0,0 +1,27 @@
+package com.shengdangjia.hermesaccount.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DirectRabbitConfig {
+
+    @Bean
+    public Queue TestDirectQueue() {
+        return new Queue("TestDirectQueue", true);
+    }
+
+    @Bean
+    DirectExchange TestDirectExchange() {
+        return new DirectExchange("TestDirectExchange");
+    }
+
+    @Bean
+    Binding bindingDirect() {
+        return BindingBuilder.bind(TestDirectQueue()).to(TestDirectExchange()).with("TestDirectRouting");
+    }
+}
--- hermes-account/src/main/java/com/shengdangjia/hermesaccount/controller/AccountController.java
+++ hermes-account/src/main/java/com/shengdangjia/hermesaccount/controller/AccountController.java
@@ -4,12 +4,16 @@ import com.shengdangjia.common.model.ErrorCode;
 import com.shengdangjia.common.model.ResponseData;
 import com.shengdangjia.common.utility.RestHelper;
 import com.shengdangjia.hermesaccount.business.AccountBusiness;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 
 @RequestMapping(value = "/account")
@@ -20,6 +24,9 @@ public class AccountController {
 
     @Autowired
     HttpServletRequest request;
+
+    @Autowired
+    RabbitTemplate rabbitTemplate;
 
     /**
      * 获取用户列表
@@ -44,6 +51,12 @@ public class AccountController {
     @RequestMapping("/find")
     ResponseData find(@RequestParam(value = "telephone") String telephone) {
         var r = accountBusiness.findByTelephone(telephone);
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("messageId", UUID.randomUUID().toString());
+        map.put("telephone", telephone);
+        rabbitTemplate.convertAndSend("TestDirectExchange", "TestDirectRouting", map);
+
         return RestHelper.makeResponse(r, ErrorCode.SUCCESS);
     }
 }
